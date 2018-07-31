58454f4a4896d149594a4c3fd54302001021b43d
José Támara <jochitoel1@hotmail.com>
2018-07-31 16:00:54 -0500

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,17 +9,57 @@ import { Link } from 'react-router-dom';
     const minLength = (len) => (val) => !(val) || (val.length >= len);
     const maxLength = (len) => (val) => val && (val.length<= len);
 
-    class DishDetail extends Component {
+    function RenderDish ({dish}){
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+    function RenderComments ({comments, addComment, dishId}) {
+        if(comments != null){
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map( (comment) => {
+                            return(
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p> 
+                                    <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <CommentForm dishId={dishId} addComment={addComment} />
+                </div>
+            );
+        }else {
+            return(
+                <div></div>
+            )
+        }
+    }
+
+    class CommentForm extends Component {
         constructor(props){
             super(props);
             this.state = {
                 isModalOpen: false
             };
             this.toggleModal = this.toggleModal.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
         }
         handleSubmit(values){
-            console.log("Current State is: "+JSON.stringify(values));
-            alert("Current State is: "+JSON.stringify(values));
+            this.toggleModal();
+            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
          
         }
         toggleModal(){
@@ -28,68 +68,10 @@ import { Link } from 'react-router-dom';
             })
         }
         
-
         render(){
-
-            const RenderDish = ({dish}) => {
-                return(
-                    <div >
-                        <Card>
-                            <CardImg width="100%" src={dish.image} alt={dish.name} />
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                );
-            }
-            const RenderComments = ({comments})=> {
-                if(comments != null){
-                    return(
-                        <div >
-                            <h4>Comments</h4>
-                            <ul className="list-unstyled">
-                                {comments.map( (comment) => {
-                                    return(
-                                        <li key={comment.id}>
-                                            <p>{comment.comment}</p> 
-                                            <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </div>
-                    );
-                }else {
-                    return(
-                        <div>ttt</div>
-                    )
-                }
-            }
-
-            if(this.props.dish !=null)
             return(
-                <div className="container">
-                    <div className="row">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{this.props.dish.name}</h3>
-                            <hr />
-                        </div>                
-                    </div>
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderDish dish={this.props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderComments comments={this.props.comments} />
-                            <Button outline className="seconday" onClick={this.toggleModal}><i className="fa fa-pencil fa-lg"></i> Submit Comment</Button>
-                        </div>
-                    </div>
+                <div >
+                    <Button outline className="seconday" onClick={this.toggleModal}><i className="fa fa-pencil fa-lg"></i> Submit Comment</Button>
                     <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                         <ModalHeader toggle={this.toggleModal}>Sunmit Comment</ModalHeader>
                         <ModalBody>
@@ -109,8 +91,8 @@ import { Link } from 'react-router-dom';
                                 </Row>
                             <Row className="form-group">
                                     <Col md={12}>
-                                        <Label htmlFor="yourname">Your Name</Label>
-                                        <Control.text model=".yourname" id="yourname" name="yourname"
+                                        <Label htmlFor="author">Your Name</Label>
+                                        <Control.text model=".author" id="author" name="author"
                                         placeholder="Your Name"
                                         className="form-control"
                                         validators={{
@@ -118,7 +100,7 @@ import { Link } from 'react-router-dom';
                                         }}/>
                                         <Errors 
                                         className="text-danger"
-                                        model=".yourname"
+                                        model=".author"
                                         show="touched"
                                         messages={{
                                             required: 'Required. ',
@@ -148,12 +130,35 @@ import { Link } from 'react-router-dom';
                     </Modal>
                 </div>
             );
-            else 
-                return(
-                    <div></div>
-                )
         }
 
+    }
+    const DishDetail = (props) => {
+        if(props.dish !=null)
+        return(
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments} 
+                           addComment={props.addComment}
+                           dishId={props.dish.id} />
+                </div>
+            </div>
+        );
+        else 
+            return(
+                <div></div>
+            )
     }
 
 
