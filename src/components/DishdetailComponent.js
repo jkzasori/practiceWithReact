fdb2587e22ea25a531df50f78bca85134011dd2d
José Támara <jochitoel1@hotmail.com>
2018-08-01 22:09:15 -0500

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,7 +25,7 @@ import { baseUrl } from '../shared/baseUrl';
         );
     }
 
-    function RenderComments ({comments, addComment, dishId}) {
+    function RenderComments ({comments, postComment, dishId}) {
         if(comments != null){
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -40,7 +40,7 @@ import { baseUrl } from '../shared/baseUrl';
                             );
                         })}
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
             );
         }else {
@@ -61,7 +61,7 @@ import { baseUrl } from '../shared/baseUrl';
         }
         handleSubmit(values){
             this.toggleModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
          
         }
         toggleModal(){
@@ -170,7 +170,7 @@ import { baseUrl } from '../shared/baseUrl';
                 <div className="row">
                         <RenderDish dish={props.dish} />
                         <RenderComments comments={props.comments} 
-                           addComment={props.addComment}
+                           postComment={props.postComment}
                            dishId={props.dish.id} />
                 </div>
             </div>
